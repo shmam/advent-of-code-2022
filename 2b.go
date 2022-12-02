a527d761697450874e00c14a9eed5e88de650cfd
shmam <samuel.d.crochet@gmail.com>
2022-12-02 18:48:04 -0500

    finished 2

---
+++ 2b.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var score = [3][3]int{
+	{3, 4, 8},
+	{1, 5, 9},
+	{2, 6, 7},
+}
+
+var translation = map[string]int{
+	"A": 0,
+	"B": 1,
+	"C": 2,
+	"X": 0,
+	"Y": 1,
+	"Z": 2,
+}
+
+func calculateScore(opponentInput, playerInput string) int {
+	return score[translation[opponentInput]][translation[playerInput]]
+}
+
+func main() {
+	fileContent := readFileReturnArray("./inputs/2.input")
+
+	gameTotal := 0
+	for _, eachLine := range fileContent {
+		splitInput := strings.Fields(eachLine)
+
+		gameTotal += calculateScore(splitInput[0], splitInput[1])
+	}
+
+	fmt.Println("Game Total: ", gameTotal)
+}
